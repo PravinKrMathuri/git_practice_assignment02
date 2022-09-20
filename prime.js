4dc92c0487adecb77ace734ecf5ab1b648a8ffd3
Pravin Kumar Mathuri <pravinkumarmathuri@gmail.com>
2022-09-20 21:58:25 +0530

    first_edition

---
+++ prime.js
@@ -0,0 +1,16 @@
+function checkPrime(num){
+    
+    let count = 0;
+    for(i=2;i<=num;i++){
+        if(num % i == 0){
+            count ++ ;
+        }
+    }
+}
+
+
+if(count == 2){
+    console.log("Prime");
+}else {
+    console.log("Not a Prime");
+}
